1fe4eed0faaf35e94f43bb89d2d9b9b248e56a7c
Nkateko Maluleke <nkateko.maluleke03@gmail.com>
2024-04-08 02:26:34 +0200

    Browser route removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 //import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/landing-page/Home";
 import Register from "./pages/Register";
@@ -9,13 +9,11 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter basename="/Data-Spider-React">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
