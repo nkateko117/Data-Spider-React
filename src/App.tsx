c52bae52c85ce55bdc319e3c79e9ee0cb8ccd188
Nkateko Maluleke <nkateko.maluleke03@gmail.com>
2024-04-08 02:14:00 +0200

    browser router included

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 //import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/landing-page/Home";
 import Register from "./pages/Register";
@@ -9,13 +9,13 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename="/vite-deploy">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
